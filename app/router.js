4519bcea383d4c1aabffe7b3432c4094d3bbb9f5
Perry Brandiezs <pbrandiezs@gmail.com>
2019-09-26 09:33:34 -0700

    fix: Added back in the rootURL previously commented out

--- app/router.js
+++ app/router.js
@@ -1,11 +1,9 @@
 import EmberRouter from '@ember/routing/router';
-//import Ember from 'ember';
 import config from './config/environment';
 
 const Router = EmberRouter.extend({
-//const Router = Ember.Router.extend({
   location: config.locationType,
-  //rootURL: config.rootURL
+  rootURL: config.rootURL
 });
 
 Router.map(function() {
