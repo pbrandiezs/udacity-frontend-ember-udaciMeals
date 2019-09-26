f74a7b28ad91ca8c46f2d57258b0b0257e16285b
Perry Brandiezs <pbrandiezs@gmail.com>
2019-09-26 09:39:43 -0700

    feat: Added model to router.js

--- app/router.js
+++ app/router.js
@@ -3,7 +3,10 @@ import config from './config/environment';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
+  model() {
+    return EmberRouter.$.get( '/menu/menu.json');
+  }
 });
 
 Router.map(function() {
