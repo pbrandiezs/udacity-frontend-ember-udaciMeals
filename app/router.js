2ed283a9a1323ff2b3c2ee28c5f6f65c8f04e8f1
Perry Brandiezs <pbrandiezs@gmail.com>
2019-09-26 10:51:32 -0700

    fix: working on the routes

--- app/router.js
+++ app/router.js
@@ -1,19 +1,13 @@
-import EmberRouter from '@ember/routing/router';
-import config from './config/environment';
-
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL,
-  model() {
-    return EmberRouter.$.get( '/menu/menu.json');
-  }
+import EmberRouter from '@ember/routing/router';
+import config from './config/environment';
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
 });
-
-Router.map(function() {
-  this.route('menu');
-  this.route('item', {path: 'item/:item_name'}, function() {
-    this.route('nutrition', { path: '/:item_name/nutrition'});
-  });
+Router.map(function() {
+  this.route('menu');
+  this.route('item', {path: 'item/:item_name'}, function() {
+    this.route('nutrition', { path: '/:item_name/nutrition'});
+  });
 });
-
-export default Router;
+export default Router;
